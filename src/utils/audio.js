22873d8c7ac19945e12aa8f1da2fd730b6d879b3
Pavel Davydov <typical000@gmail.com>
2017-05-25 01:25:42 +0300

    Add audio repeat

--- src/utils/audio.js
+++ src/utils/audio.js
@@ -5,6 +5,7 @@ export const loadAudio = url => new Promise((resolve) => {
   const audio = new Audio()
   audio.src = url
   audio.preload = 'auto'
+  audio.loop = true
   audio.addEventListener('loadeddata', resolve.call(this, audio))
 })
 
